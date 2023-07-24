b5880404b0ecbe737c2649e13ec5d6d1841e4268
GiftChrisJava <chrisjava77@gmail.com>
2023-07-24 12:45:53 -0700

    added a route for creating traveler acount

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -3,6 +3,6 @@ const AccoutController = require("../controller/accoutController");
 const router = express.Router();
 
 router.post("/register/admin", AccoutController.createAdminAccount);
-router.post("/register/traveler", AccoutController.createAdminAccount);
+router.post("/register/traveler", AccoutController.createTravelerAccount);
 
 module.exports = router;
