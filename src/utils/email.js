d2b27f4f52705cadfd0a9e45775c39a8c46546df
GiftChrisJava <chrisjava77@gmail.com>
2023-08-01 11:07:56 -0700

    send multiple emails function

--- src/utils/email.js
+++ src/utils/email.js
@@ -22,4 +22,19 @@ const sendWelcomeEmail = async (email) => {
   }
 };
 
-module.exports = { sendWelcomeEmail };
+const sendMultipleEmails = async (traveler) => {
+  const firstName = traveler.firstName;
+  const email = traveler.email;
+  try {
+    await transporter.sendMail({
+      from: EMAIL,
+      to: email,
+      subject: "Hey" + firstName + ", Welcome to Simpoft Bus booking System",
+      text: "Thank you for each and every help you offered to me last semi. I appriate you it a lot not matter how small it seemed. AM CODING A BUS BOOKING SYSTEM AND I WANT IT TO BE SENDING EMAILS TO TRAVELERS",
+    });
+  } catch (error) {
+    console.error("Error sending Welcome email : ", error);
+  }
+};
+
+module.exports = { sendWelcomeEmail, sendMultipleEmails };
