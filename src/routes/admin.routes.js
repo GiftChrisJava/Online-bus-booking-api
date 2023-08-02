7b790fb24fa369d1a077823e3b3abd56bd3df01a
GiftChrisJava <chrisjava77@gmail.com>
2023-08-02 02:14:13 -0700

    added admin location controller routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const busController = require("../controller/busController");
 const adminMiddleware = require("../middleware/AdminMiddleware");
+const locationController = require("../controller/locationController");
 const router = express.Router();
 
 // controlling the bus
@@ -10,5 +11,12 @@ router.get("/buses", busController.getBuses);
 router.get("/bus/:busId", busController.getBus);
 router.delete("/bus/:busId", busController.deleteBus);
 router.put("/bus/busId", busController.updateBus);
+
+// controlling bus routes
+router.post("/route", locationController.createLocation);
+router.put("/route/:locationId", locationController.updateBusRoute);
+router.delete("/route/:locationId", locationController.deleteBusRoute);
+router.get("/routes", locationController.getBusRoutes);
+router.get("/locations", locationController.getBusLocations);
 
 module.exports = router;
