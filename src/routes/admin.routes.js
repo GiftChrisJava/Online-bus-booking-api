25db61fb6a42721b9c3acab6b3d798c11c0956d2
GiftChrisJava <chrisjava77@gmail.com>
2023-08-02 01:52:28 -0700

    added admin bus controller routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,4 +4,11 @@ const adminMiddleware = require("../middleware/AdminMiddleware");
 const router = express.Router();
 
 // controlling the bus
-router.post("/bus", adminMiddleware, busController.createBus);
+router.post("/bus", busController.createBus);
+router.get("/bus/location", busController.getBusLocationDetails);
+router.get("/buses", busController.getBuses);
+router.get("/bus/:busId", busController.getBus);
+router.delete("/bus/:busId", busController.deleteBus);
+router.put("/bus/busId", busController.updateBus);
+
+module.exports = router;
