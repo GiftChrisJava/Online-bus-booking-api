db6207889d87a7759029f0d71e5a105129825e6a
GiftChrisJava <chrisjava77@gmail.com>
2023-08-06 12:09:39 -0700

    added admin controller

---
+++ src/controller/adminController.js
@@ -0,0 +1,51 @@
+const AdminService = require("../service/adminService");
+
+const adminController = {
+  // cancel booking
+  cancelBooking: async (req, res) => {
+    const { travelerId } = req.params;
+    try {
+      const result = await AdminService.cancelBooking(travelerId);
+
+      if (result.error) {
+        return res.status(409).json({ error: result.error });
+      }
+
+      return res.status(200).json({ msg: result.msg });
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  },
+
+  // get all the travelers who paid
+  getTravelersWithPayments: async (req, res) => {
+    try {
+      const result = await AdminService.getTravelersWithPayments();
+
+      if (result.error) {
+        return res.status(409).json({ error: result.error });
+      }
+
+      return { travelers: result.travelers };
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  },
+
+  // get all the travelers who have not paid for their ticket
+  getTravelersWithoutPayments: async (req, res) => {
+    try {
+      const result = await AdminService.getTravelersWithoutPayments();
+
+      if (result.error) {
+        return res.status(409).json({ error: result.error });
+      }
+
+      return { travelers: result.travelers };
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  },
+};
+
+module.exports = adminController;
