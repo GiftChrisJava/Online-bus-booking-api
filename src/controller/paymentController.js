2bd216c0316a74b1aa6f8d3a9a5a7ae97318a2fc
GiftChrisJava <chrisjava77@gmail.com>
2023-08-07 21:35:00 -0700

    possible to process payments

--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -2,21 +2,19 @@ const PaymentService = require("../service/PaymentService");
 
 const paymentController = {
   processPayment: async (req, res) => {
-    const { paymentData, token } = req.body;
+    try {
+      const { paymentData, token } = req.body;
 
-    // const result = await PaymentService.processPayment(
-    //   // cvv,
-    //   req.body
-    //   // paymentDate
-    // );
+      const result = await PaymentService.processPayment(paymentData, token);
 
-    const result = await PaymentService.processPayment(paymentData, token);
+      if (result.error) {
+        return res.status(409).json({ error: result.error });
+      }
 
-    if (result.error) {
-      return res.status(409).json({ error: result.error });
+      return res.status(201).json({ bus: result.payment });
+    } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
     }
-
-    return res.status(201).json({ bus: result.payment });
   },
 };
 
--- src/service/PaymentService.js
+++ src/service/PaymentService.js
@@ -5,54 +5,64 @@ const stripe = require("stripe")(
 const entities = require("../models");
 const Payment = entities.Payment;
 const Ticket = entities.Ticket;
+const Location = entities.Location;
 
 const PaymentService = {
   async processPayment(paymentData, token) {
     const travelerId = paymentData.travelerId;
 
-    // Create a Stripe charge
-    const stripeCharge = await stripe.charges.create({
-      amount: amount * 100, // Stripe requires amount in cents
-      currency: "mwk", // Malawi Kwacha
-      source: token,
-      description: "Payment for Ticket",
-    });
+    try {
+      // calculate total payment
+      let amount = 0;
 
-    if (!stripeCharge) {
-      return { error: "Stripe charge never happened" };
+      const tickets = await Ticket.findAll({ where: { travelerId } });
+
+      // for each ticket update paidForBookedBus to true
+      for (const ticket of tickets) {
+        // update
+        await ticket.update({ paidForBookedBus: true });
+
+        let busId = ticket.busId;
+
+        // find a location with specified busId
+        const location = await Location.findOne({ where: { busId: busId } });
+        if (location) {
+          amount = amount + location.cost;
+        } else {
+          console.log("Location not found for the specified busId.");
+        }
+      }
+
+      // Create a Stripe charge
+      const stripeCharge = await stripe.charges.create({
+        amount: amount * 100, // Stripe requires amount in cents
+        currency: "mwk", // Malawi Kwacha
+        source: token,
+        description: "Payment for Ticket",
+      });
+
+      if (!stripeCharge) {
+        return { error: "Stripe charge never happened" };
+      }
+
+      // Create a payment record in the database
+      const payment = await Payment.create({
+        paymentDate: new Date(),
+        paymentAmount: amount,
+        paymentMethod: "card",
+        travelerId: travelerId,
+      });
+
+      if (!payment) {
+        return { error: "Payment records not created" };
+      }
+
+      console.log(tickets);
+
+      return { payment };
+    } catch (error) {
+      throw new Error("Something went wrong in the code");
     }
-
-    // Create a payment record in the database
-    const payment = await Payment.create({
-      // paymentDate: new Date(paymentDate),
-      paymentAmount: amount,
-      paymentMethod: "card",
-    });
-
-    if (!payment) {
-      return { error: "Payment records not created" };
-    }
-
-    // calculate total payment
-    let amount = 0;
-
-    const tickets = await Ticket.findAll({ where: { travelerId } });
-
-    // for each ticket update paidForBookedBus to true
-    for (const ticket of tickets) {
-      // update
-      await ticket.update({ paidForBookedBus: true });
-
-      let busId = ticket.busId;
-
-      // find a location with specified busId
-      const location = entities.Location.findOne({ where: { busId } });
-      amount = amount + location.cost;
-    }
-
-    console.log(tickets);
-
-    return { payment };
   },
 };
 
