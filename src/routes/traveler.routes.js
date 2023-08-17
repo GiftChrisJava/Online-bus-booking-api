f5116f49191b0d0ad9c9a7d27821d0159e6e544e
GiftChrisJava <chrisjava77@gmail.com>
2023-08-17 04:39:11 -0700

    debugging

--- src/routes/traveler.routes.js
+++ src/routes/traveler.routes.js
@@ -1,15 +1,15 @@
-// const express = require("express");
-// const travelerController = require("../controller/travelerController");
-// const paymentController = require("../controller/paymentController");
-// const router = express.Router();
+const express = require("express");
+const travelerController = require("../controller/travelerController");
+const paymentController = require("../controller/paymentController");
+const router = express.Router();
 
-// router.get("/search", travelerController.searchBus);
-// router.get(
-//   "/bookticket/:travelerId/:busId/:seatNumber",
-//   travelerController.bookTicket
-// );
-// router.post("/create", travelerController.createTraveler);
-// router.post("/pay", paymentController.processPayment);
-// router.delete("/cancel-booking", travelerController.cancelBooking);
+router.get("/search", travelerController.searchBus);
+router.get(
+  "/bookticket/:travelerId/:busId/:seatNumber",
+  travelerController.bookTicket
+);
+router.post("/create", travelerController.createTraveler);
+router.post("/pay", paymentController.processPayment);
+router.delete("/cancel-booking", travelerController.cancelBooking);
 
-// module.exports = router;
+module.exports = router;
