56cf513ad5935c19a71c2f019df73ec2ff2d5205
GiftChrisJava <chrisjava77@gmail.com>
2023-08-28 13:15:28 -0700

    testing payment

--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -3,23 +3,19 @@ const email = require("../utils/email");
 
 const paymentController = {
   processPayment: async (req, res) => {
-    try {
-      const { paymentData, paymentMethodId } = req.body;
+    const { paymentData, paymentMethodId } = req.body;
 
-      const result = await PaymentService.processPayment(
-        paymentData,
-        paymentMethodId
-      );
+    const result = await PaymentService.processPayment(
+      paymentData,
+      paymentMethodId
+    );
 
-      if (result.error) {
-        return res.status(409).json({ error: result.error });
-      }
-
-      console.log(result.tickets);
-      return res.status(201).json({ bus: result.payment });
-    } catch (error) {
-      return res.status(500).json({ error: "Internal server error" });
+    if (result.error) {
+      return res.status(409).json({ error: result.error });
     }
+
+    console.log(result.tickets);
+    return res.status(201).json({ bus: result.payment });
   },
 };
 
--- src/service/PaymentService.js
+++ src/service/PaymentService.js
@@ -17,109 +17,103 @@ const PaymentService = {
     const travelerId = paymentData.travelerId;
     const travelDate = paymentData.travelDate;
 
-    try {
-      // calculate total payment
-      let amount = 0;
+    // calculate total payment
+    let amount = 0;
 
-      const tickets = await Ticket.findAll({
-        where: { travelerId },
-        include: [
-          {
-            model: Bus,
-            include: [
-              { model: Location, where: { departureDate: travelDate } },
-            ],
-          },
-          { model: Seat },
-        ],
-      });
+    const tickets = await Ticket.findAll({
+      where: { travelerId },
+      include: [
+        {
+          model: Bus,
+          include: [{ model: Location, where: { departureDate: travelDate } }],
+        },
+        { model: Seat },
+      ],
+    });
 
-      let busId;
-      // calculate cost
-      for (const ticket of tickets) {
-        busId = ticket.busId;
+    let busId;
+    // calculate cost
+    for (const ticket of tickets) {
+      busId = ticket.busId;
 
-        // find a location with specified busId
-        const location = await Location.findOne({ where: { busId: busId } });
-        if (location) {
-          amount = amount + location.cost;
-        } else {
-          console.log("Location not found for the specified busId.");
-        }
-      }
-
-      // find a seat and update availability status
-      const seats = await Seat.findAll({
-        where: { travelerId, isAvailable: true },
-      });
-
-      // if available
-      if (seats) {
-        // Create a Stripe payment intent
-        const paymentIntent = await stripe.paymentIntents.create({
-          amount: amount * 100, // Stripe requires amount in cents
-          currency: "mwk", // Malawi Kwacha
-          payment_method: paymentMethodId, // Use paymentMethodId instead of source
-          description: "Payment for Ticket",
-          confirm: true, // Confirm the payment intent immediately
-        });
-
-        if (!paymentIntent) {
-          return { error: "Stripe payment intent never happened" };
-        }
-
-        // update seats
-        const updatedSeats = seats.map((seat) => {
-          seat.update({ isAvailable: false });
-        });
-
-        // mark that the ticket has been paid for
-        const updatedTicket = tickets.map((ticket) => {
-          ticket.update({ paidForBookedBus: true });
-        });
+      // find a location with specified busId
+      const location = await Location.findOne({ where: { busId: busId } });
+      if (location) {
+        amount = amount + location.cost;
       } else {
-        return {
-          error: "Seats have already been taken before you made payment",
-        };
+        console.log("Location not found for the specified busId.");
       }
-
-      // Create a payment record in the database
-      const payment = await Payment.create({
-        paymentDate: new Date(),
-        paymentAmount: amount,
-        paymentMethod: "card",
-        travelerId: travelerId,
-      });
-
-      if (!payment) {
-        return { error: "Payment records not created" };
-      }
-
-      const traveler = await Traveler.findOne({ where: { id: travelerId } });
-
-      // send email
-      email.sendTicketInformation(tickets, traveler.email);
-
-      // create a booking history
-      const booking = await Booking.create({
-        travelerId: travelerId,
-        busId: busId,
-        travelerEmail: traveler.email,
-        travelerContact: traveler.contact,
-        route: tickets[0].Location.route,
-        departureDate: tickets[0].Location.departureDate,
-        departureTime: tickets[0].Location.departureTime,
-        paymentDate: payment.paymentDate,
-        paymentAmount: payment.paymentAmount,
-        paymentMethod: payment.paymentMethod,
-      });
-
-      console.log(booking);
-
-      return { payment, tickets };
-    } catch (error) {
-      throw new Error("something went wrong");
     }
+
+    // find a seat and update availability status
+    const seats = await Seat.findAll({
+      where: { travelerId, isAvailable: true },
+    });
+
+    // if available
+    if (seats) {
+      // Create a Stripe payment intent
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount * 100, // Stripe requires amount in cents
+        currency: "mwk", // Malawi Kwacha
+        payment_method: paymentMethodId, // Use paymentMethodId instead of source
+        description: "Payment for Ticket",
+        confirm: true, // Confirm the payment intent immediately
+      });
+
+      if (!paymentIntent) {
+        return { error: "Stripe payment intent never happened" };
+      }
+
+      // update seats
+      const updatedSeats = seats.map((seat) => {
+        seat.update({ isAvailable: false });
+      });
+
+      // mark that the ticket has been paid for
+      const updatedTicket = tickets.map((ticket) => {
+        ticket.update({ paidForBookedBus: true });
+      });
+    } else {
+      return {
+        error: "Seats have already been taken before you made payment",
+      };
+    }
+
+    // Create a payment record in the database
+    const payment = await Payment.create({
+      paymentDate: new Date(),
+      paymentAmount: amount,
+      paymentMethod: "card",
+      travelerId: travelerId,
+    });
+
+    if (!payment) {
+      return { error: "Payment records not created" };
+    }
+
+    const traveler = await Traveler.findOne({ where: { id: travelerId } });
+
+    // send email
+    email.sendTicketInformation(tickets, traveler.email);
+
+    // create a booking history
+    const booking = await Booking.create({
+      travelerId: travelerId,
+      busId: busId,
+      travelerEmail: traveler.email,
+      travelerContact: traveler.contact,
+      route: tickets[0].Location.route,
+      departureDate: tickets[0].Location.departureDate,
+      departureTime: tickets[0].Location.departureTime,
+      paymentDate: payment.paymentDate,
+      paymentAmount: payment.paymentAmount,
+      paymentMethod: payment.paymentMethod,
+    });
+
+    console.log(booking);
+
+    return { payment, tickets };
   },
 };
 
